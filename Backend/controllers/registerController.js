caa299a25dd413f3bde8a49a7997fb87ab0b6e85
Preet <2000vermapreet@gmail.com>
2023-02-15 20:10:02 +0530

    api addded

--- Backend/controllers/registerController.js
+++ Backend/controllers/registerController.js
@@ -1,15 +1,19 @@
 let mongoose = require('mongoose');
 const register = require('../models/mongooseSchema/register');
 
-exports.registerUser = async (req,res)=>{
+exports.registerUser = (req,res)=>{
 const data= new register({
     userName:req.body.userName,
     name:req.body.name,
     password:req.body.password,
     email:req.body.email
 })
-await data.save();
-res.send(data)
+ data.save().then(data=>{
+    res.json(data)
+ }).catch(error=>{
+    res.send(error);
+ })
+// res.send(data)
 }
 
 exports.fetchUsers = async (req,res)=>{
